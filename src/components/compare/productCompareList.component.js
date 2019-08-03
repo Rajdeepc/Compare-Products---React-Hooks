82234478ff9793ae4de1a1ad5019c5a5b59f05db
Rajdeepc <rajrock38@gmail.com>
2019-08-03 21:06:19 +0530

    new logic to add

--- src/components/compare/productCompareList.component.js
+++ src/components/compare/productCompareList.component.js
@@ -2,49 +2,84 @@ import React from 'react'
 import { connect } from 'react-redux';
 import { removeProductFromCompare } from './compare.action';
 import { Row, Col, Card, Button } from 'react-bootstrap';
+import addToCompare from './add.png';
+
 
 const ProductCompareList = (props) => {
 
     let prodArray = props.addRemoveState.compareArray;
-
+    let compareArray = [
+        {
+            "imageAdd": addToCompare
+        },
+        {
+            "imageAdd": addToCompare
+        },
+        {
+            "imageAdd": addToCompare
+        },
+        {
+            "imageAdd": addToCompare
+        }
+    ];
 
     const removeItem = (indexToDelete) => {
         props.removeProductFromCompare(indexToDelete);
     }
+    const popImageAndPushItem = () => {
+        let indexOfLastItem = prodArray.indexOf(prodArray[prodArray - 1]);
+        console.log("indexOfLastItem" + indexOfLastItem)
+        prodArray.map((item, index) => {
+            debugger;
+            compareArray.splice(indexOfLastItem + 1, 1, item);
+            indexOfLastItem++;
+        })
+        console.log("compareArray" + compareArray)
+        return compareArray;
+    }
 
     return (
         <div>
-        <h4>{prodArray.length ? 'Compare List' : ''}</h4>
-        <Row>
-           
-            {prodArray.map((item, index) => {
-                return (
-                    <Col sm={3}>
-                        <Card>
-                            <div style={{ width: '10em', height: '10em', margin: '0 auto' }}>
-                                <img style={{ height: 'auto', maxWidth: '100%' }} src={item.image} />
-                            </div>
+            <h4>{prodArray.length ? 'Compare List' : ''}</h4>
+            <Row>
+                {popImageAndPushItem().map((item, index) => {
+                    return (
+                        <Col sm={3}>
+                            <Card>
+                                {item.price && item.description && item.name && item.condition && item.image ?
+                                    <>
+                                        <div style={{ width: '10em', height: '10em', margin: '0 auto' }}>
+                                            <img style={{ height: 'auto', maxWidth: '100%' }} src={item.image} />
+                                        </div>
 
-                            <Card.Body>
-                                <Card.Title>{item.name}</Card.Title>
-                                <Card.Text>
-                                    {item.description}
-                                </Card.Text>
-                                <Card.Text>
-                                   Price:  {item.price}
-                                </Card.Text>
-                                <Card.Text>
-                                   Condition {item.condition}
-                                </Card.Text>
-                                <Button
-                                    variant="danger" onClick={() => removeItem(index)}>Remove</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
 
-                )
-            })}
-        </Row>
+                                        <Card.Body>
+                                            <Card.Title>{item.name}</Card.Title>
+                                            <Card.Text>
+                                                {item.description}
+                                            </Card.Text>
+                                            <Card.Text>
+                                                Price:  {item.price}
+                                            </Card.Text>
+                                            <Card.Text>
+                                                Condition {item.condition}
+                                            </Card.Text>
+                                            <Button
+                                                variant="danger" onClick={() => removeItem(index)}>Remove</Button>
+
+
+                                        </Card.Body>
+                                    </> :
+                                    <div style={{ width: '10em', height: '10em', margin: '0 auto' }}>
+                                        <img style={{ height: 'auto', maxWidth: '100%' }} src={item.imageAdd} />
+                                    </div>
+                                }
+                            </Card>
+                        </Col>
+                    )
+                })
+                }
+            </Row>
         </div>
     )
 }
